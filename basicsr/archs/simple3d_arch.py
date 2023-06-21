ceb849bdf160711ea8517bca0645397b79ba15aa
Piper Wolters <piperw@allenai.org>
2023-06-21 18:34:58 +0000

    simple 3d

---
+++ basicsr/archs/simple3d_arch.py
@@ -0,0 +1,38 @@
+import os
+import torch
+from collections import OrderedDict
+from torch import nn as nn
+
+from basicsr.utils.registry import ARCH_REGISTRY
+
+
+@ARCH_REGISTRY.register()
+class Simple3DConvNet(nn.Module):
+    def __init__(self, num_in_ch, num_out_ch, scale=4, num_feat=64, num_block=23, num_grow_ch=32)::
+        super(Simple3DConvNet, self).__init__()
+
+        self.conv1 = nn.Conv3d(num_in_chan, num_in_chan+num_grow_ch, kernel_size=3, padding=1)
+        self.relu = nn.ReLU()
+        self.pool = nn.MaxPool3d(kernel_size=2, stride=2)
+        self.conv2 = nn.Conv3d(num_in_chan+num_grow_ch, num_in_chan+num_grow_ch*2, kernel_size=3, padding=1)
+
+        self.fc = nn.Linear(32 * 8 * 8 * 8, num_out_ch)
+
+    def forward(self, x):
+        print("x:", x.shape)
+        out = self.conv1(x)
+        print("Conv1 output shape:", out.shape)
+        out = self.relu(out)
+        out = self.pool(out)
+        print("Pool1 output shape:", out.shape)
+        out = self.conv2(out)
+        print("Conv2 output shape:", out.shape)
+        out = self.relu(out)
+        out = self.pool(out)
+        print("Pool2 output shape:", out.shape)
+        out = out.view(out.size(0), -1)
+        print("out:", out.shape)
+        out = self.fc(out)
+        print("Final output shape:", out.shape)
+        return out
+
