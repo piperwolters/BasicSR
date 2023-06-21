8c97245b20d20f12ba7e4444eff99b2c5bce5116
Piper Wolters <piperw@prior-cirrascale-74.reviz.ai2.in>
2023-06-21 22:17:33 +0000

    delete prints

--- basicsr/archs/simple3d_II_arch.py
+++ basicsr/archs/simple3d_II_arch.py
@@ -6,7 +6,7 @@ from basicsr.utils.registry import ARCH_REGISTRY
 
 @ARCH_REGISTRY.register()
 class Simple3DConvNetII(nn.Module):
-    def __init__(self, num_in_ch, num_out_ch, scale=4, num_feat=512, num_block=23, num_grow_ch=64):
+    def __init__(self, num_in_ch, num_out_ch, scale=4, num_feat=64, num_block=23, num_grow_ch=64):
         super(Simple3DConvNetII, self).__init__()
 
         self.conv1 = nn.Conv3d(num_in_ch, num_feat, kernel_size=(3, 1, 1), stride=(1, 1, 1), padding=(1, 0, 0))
@@ -26,45 +26,28 @@ class Simple3DConvNetII(nn.Module):
         self.up3 = nn.ConvTranspose2d(num_feat*2, 3, kernel_size=1, stride=1)
 
     def forward(self, x):
-        print("input:", x.shape)
         out = self.conv1(x)
-        print("Conv1:", out.shape)
         out = self.pool(self.relu(out))
-        print("Pool1:", out.shape)
         
         out = self.conv15(out)
-        print("Conv 1.5:", out.shape)
 
         out = self.conv2(out)
-        print("Conv2:", out.shape)
         out = self.pool(self.relu(out))
-        print("Pool2:", out.shape)
         out = self.conv25(out)
-        print("Conv 2.5:", out.shape)
 
         out = self.conv3(out)
-        print("Conv3:", out.shape)
         out = self.pool(self.relu(out))
-        print("Pool3:", out.shape)
         out = self.conv35(out)
-        print("Conv 3.5:", out.shape)
 
         out = self.conv4(out)
-        print("Conv4:", out.shape)
         out = self.pool(self.relu(out))
-        print("Pool4:", out.shape)
         out = self.conv45(out)
-        print("Conv 4.5:", out.shape)
 
         out = out.squeeze(2)
-        print("Temporal Dim Squeeze:", out.shape)
 
         up = self.up1(out)
-        print("Up1:", up.shape)
         up = self.up2(up)
-        print("Up2:", up.shape)
         up = self.up3(up)
-        print("Up3:", up.shape)
 
         return up
 
