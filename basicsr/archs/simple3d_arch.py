714638727751c39021cbf14d74c3c206987e8928
Piper Wolters <piperw@allenai.org>
2023-06-21 19:00:28 +0000

    upsample

--- basicsr/archs/simple3d_arch.py
+++ basicsr/archs/simple3d_arch.py
@@ -8,7 +8,7 @@ from basicsr.utils.registry import ARCH_REGISTRY
 
 @ARCH_REGISTRY.register()
 class Simple3DConvNet(nn.Module):
-    def __init__(self, num_in_ch, num_out_ch, scale=4, num_feat=64, num_block=23, num_grow_ch=32):
+    def __init__(self, num_in_ch, num_out_ch, scale=4, num_feat=64, num_block=23, num_grow_ch=64):
         super(Simple3DConvNet, self).__init__()
 
         self.conv1 = nn.Conv3d(num_in_ch, num_feat, kernel_size=(3, 1, 1), stride=(1, 1, 1), padding=(1, 0, 0))
@@ -17,6 +17,10 @@ class Simple3DConvNet(nn.Module):
         self.conv2 = nn.Conv3d(num_feat, num_feat+num_grow_ch*2, kernel_size=(3, 1, 1), stride=(1, 1, 1), padding=(1, 0, 0))
         self.conv3 = nn.Conv3d(num_feat+num_grow_ch*2, num_feat+num_grow_ch*3, kernel_size=(3, 1, 1), stride=(1, 1, 1), padding=(1, 0, 0))
         self.conv4 = nn.Conv3d(num_feat+num_grow_ch*3, num_feat+num_grow_ch*4, kernel_size=(3, 1, 1), stride=(1, 1, 1), padding=(1, 0, 0))
+
+        self.up1 = nn.Conv2d(num_feat+num_grow_ch*4, num_feat+num_grow_ch*3, 3, 1, 1)
+        self.up2 = nn.Conv2d(num_feat+num_grow_ch*3, num_feat+num_grow_ch*2, 3, 1, 1)
+        self.up3 = nn.Conv2d(num_feat+num_grow_ch*2, num_feat+num_grow_ch*1, 3, 1, 1)
 
     def forward(self, x):
         print("x:", x.shape)
@@ -39,6 +43,13 @@ class Simple3DConvNet(nn.Module):
 
         out = out.squeeze(2)
         print("squeeze:", out.shape)
+
+        up = self.up1(out)
+        print("up1:", up.shape)
+        up = self.up2(up)
+        print("up2:", up.shape)
+        up = self.up3(up)
+        print("up3:", up.shape)
 
         return out
 
