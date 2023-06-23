ee6fcb48418699872811a3b38ab29c91fce54a3b
Piper Wolters <piperw@prior-cirrascale-74.reviz.ai2.in>
2023-06-23 20:00:44 +0000

    sync

--- basicsr/utils/logger.py
+++ basicsr/utils/logger.py
@@ -138,6 +138,7 @@ def init_wandb_logger(opt):
         wandb_id = wandb.util.generate_id()
         resume = 'never'
 
+    wandb.login(key=opt['logger']['wandb']['key'])
     wandb.init(id=wandb_id, resume=resume, name=opt['name'], config=opt, project=project, sync_tensorboard=True)
 
     logger.info(f'Use wandb logger with id={wandb_id}; project={project}.')
