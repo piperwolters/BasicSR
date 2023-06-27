6b0382053a76d8012d6891d25999a683b975b132
Piper Wolters <piperw@allenai.org>
2023-06-27 14:40:47 -0700

    timer

--- basicsr/archs/rrdbnet_arch.py
+++ basicsr/archs/rrdbnet_arch.py
@@ -109,6 +109,8 @@ class RRDBNet(nn.Module):
         self.lrelu = nn.LeakyReLU(negative_slope=0.2, inplace=True)
 
     def forward(self, x):
+        import time
+        print('x:', x.shape, time.perf_counter())
         if self.scale == 2:
             feat = pixel_unshuffle(x, scale=2)
         elif self.scale == 1:
@@ -129,4 +131,5 @@ class RRDBNet(nn.Module):
                 feat = self.lrelu(self.conv_up4(F.interpolate(feat, scale_factor=2, mode='nearest')))
 
         out = self.conv_last(self.lrelu(self.conv_hr(feat)))
+        print('out:', out.shape, time.perf_counter())
         return out
