61c0db04abe473870c24f75c6a9579715aa01a18
Piper Wolters <piperw@allenai.org>
2023-11-02 20:34:34 +0000

    fix

--- basicsr/losses/basic_loss.py
+++ basicsr/losses/basic_loss.py
@@ -157,8 +157,8 @@ class EvaSimLoss(nn.Module):
 
         batch_losses = []
         for b in range(x.shape[0]):
-            x_feats = self.sim_model(x[b, :, :, :])
-            gt_feats = self.sim_model(gt[b, :, :, :])
+            x_feats = self.sim_model(x[b, :, :, :].unsqueeze(0))
+            gt_feats = self.sim_model(gt[b, :, :, :].unsqueeze(0))
             l1 = l1_loss(x_feats, gt_feats)
             batch_losses.append(l1)
 
