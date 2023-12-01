d2c1d5a4481e3935fbc717c5903046006ca2ca4a
Piper Wolters <piperw@allenai.org>
2023-12-01 21:47:40 +0000

    fix argument bug

--- basicsr/metrics/clipscore.py
+++ basicsr/metrics/clipscore.py
@@ -5,14 +5,17 @@ import torch.nn.functional as F
 from basicsr.utils.registry import METRIC_REGISTRY
 
 @METRIC_REGISTRY.register()
-def calculate_clipscore(img1, img2, clip_model):
-    print("imgs:", img1.shape, img2.shape, clip_model)
+def calculate_clipscore(img, img2, clip_model='clipa', **kwargs):
+    assert img.shape == img2.shape, (f'Image shapes are different: {img.shape}, {img2.shape}.')
+
+    print("imgs:", img.shape, img2.shape, clip_model)
     if clip_model == 'clipa':
         model, _, preprocess = open_clip.create_model_and_transforms('ViT-bigG-14-CLIPA-336', pretrained='datacomp1b')
 
-    
+    img = F.interpolate(img, (224,224))
+    img2 = F.interpolate(img2, (224,224))
 
-    img1_feats = model(img1)
+    img1_feats = model(img)
     img2_feats = model(img2)
 
     sim_score = F.cosine_similarity(img1_feats, img2_feats)
